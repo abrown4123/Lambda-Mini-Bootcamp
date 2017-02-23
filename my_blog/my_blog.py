e1022fc6ea30f1906833c88e6337165284307d7a
Anthony Brown <abrown4123@gmail.com>
2017-02-22 18:10:49 -0600

    Following along with Lesson 3

--- my_blog/my_blog.py
+++ my_blog/my_blog.py
@@ -1,36 +1,39 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
+
+connection = sqlite3.connect('database.db')
+print('Open database successfully')
+
+connection.execute('CREATE TABLE IF NOT EXISTS posts (title TEXT, post TEXT)')
+print('Table created successfully')
+connection.close()
 
 
 @app.route('/')
 def index():
 	return render_template('home.html')
-	
-@app.route('/birthday')
-def birthday():
-	return "October 30 1911"
 
-@app.route('/birthday/<name>')
-def hello(name):
-	return "Hello " + name + '!'
-
-@app.route('/sum/<int:a>/<int:b>')
-def add(a, b):
-	total =  a + b;
-	return str(total)
-
-@app.route('/multiply/<int:a>/<int:b>')
-def multiply(a, b):
-	total =  a * b;
-	return str(total)
+@app.route('/new')
+def new_post():
+	return render_template('new.html')
 	
-@app.route('/subtract/<int:a>/<int:b>')
-def subtract(a, b):
-	total =  a - b;
-	return str(total)	
+@app.route('/addrecord', methods = ['POST'])
+def addrecord():
+	connection = sqlite3.connect('database.db')
+	cursor = connection.cursor()
 	
-@app.route('/favoritefoods')
-def list():
-	foods = ['pizza', 'ice cream', 'peanut butter']
-	return jsonify(foods)
+	try:
+		title = request.form['title']
+		post = request.form['post']
+		cursor.execute('INSERT INTO posts (title,post) VALUES (?,?)', (title,post))
+		connection.commit()
+		message = 'Record successfully added'
+	except:
+		connection.rollback()
+		message = 'error in insert operation'
+	finally:
+		return render_template('result.html', message = message)
+		connection.close()
+	
